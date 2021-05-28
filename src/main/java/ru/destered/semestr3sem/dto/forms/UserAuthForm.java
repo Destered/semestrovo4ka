c5b2c99723655afed644954beb6a757c6a33d7b4
Timur Boltachev <timur.boltachev@joy-dev.com>
2021-05-28 19:26:42 +0300

    chtoto sdelal

--- src/main/java/ru/destered/semestr3sem/dto/forms/UserAuthForm.java
+++ src/main/java/ru/destered/semestr3sem/dto/forms/UserAuthForm.java
@@ -5,8 +5,10 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
-
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 
 @AllArgsConstructor
@@ -15,7 +17,12 @@ import javax.validation.constraints.NotBlank;
 @Data
 public class UserAuthForm {
     @NotBlank(message = "email is mandatory")
+    @Email(message = "Input correct email")
+    @NotNull(message = "Input email")
     private String email;
+
+    @Size(min = 8, max = 20)
+    @NotNull(message = "Enter password")
     @NotBlank(message = "password is mandatory")
     private String password;
 
